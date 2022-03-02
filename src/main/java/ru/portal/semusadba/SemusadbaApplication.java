44b0f9e984546851b0b8a85dd46364cca73568b2
Olga Smirnova <olga.smirnova@tune-it.ru>
2022-03-02 22:36:49 +0300

    start

---
+++ src/main/java/ru/portal/semusadba/SemusadbaApplication.java
@@ -0,0 +1,13 @@
+package ru.portal.semusadba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemusadbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemusadbaApplication.class, args);
+	}
+
+}
